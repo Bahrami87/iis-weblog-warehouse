3141a6a41b8db32c1c2661af16d3fa3e881f6c6c
Bahrami <ziabahrami87@gmail.com>
2026-05-02 23:22:11 +0100

    chore: remove GeoIP archives and debug scripts from tracking

--- check_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('consolidated_iis_logs.csv', dtype=str, low_memory=False)
-print('Columns:', list(df.columns))
-print()
-print('sc_status values:', df['sc_status'].value_counts().head(5).to_dict())
-print()
-print('First 3 rows full:')
-print(df.head(3).to_string())
